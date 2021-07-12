d983a539f7a28a9d121c170741c50bc4d92efb35
Brooke Kullberg <brookekullberg@gmail.com>
2021-07-12 11:06:52 -0700

    full testing of Triangle with Jest

---
+++ __tests__/triangle.test.js
@@ -0,0 +1,32 @@
+import Triangle from './../src/triangle.js';
+
+describe('Triangle', () => {
+
+  test('should correctly create a triangle object with three lengths', () => {
+    const triangle = new Triangle(2,4,5);
+    expect(triangle.side1).toEqual(2);
+    expect(triangle.side2).toEqual(4);
+    expect(triangle.side3).toEqual(5);
+  });
+
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    const notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isocTriangle = new Triangle(5,5,7)
+    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+  });
+
+  test('should correctly determine whether three lengths make an scalene triangle', () => {
+    const scalTriangle = new Triangle(2,3,4)
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5)
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
+
+});
