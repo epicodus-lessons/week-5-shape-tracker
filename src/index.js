77220bb3195e17abbc698979d4efda822f775448
Brooke Kullberg <48640716+BrookeZK@users.noreply.github.com>
2022-07-07 13:44:57 -0700

    parse form values

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import Triangle from './triangle.js';
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
+    const length1 = parseInt($('#length1').val());
+    const length2 = parseInt($('#length2').val());
+    const length3 = parseInt($('#length3').val());
     const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
